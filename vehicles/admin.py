5ef160a3c14263441a3d54e3bf880d2fb72a6653
lucasnunes146 <lucasnunes146@gmail.com>
2021-12-06 13:27:08 -0300

    runing migrate

---
+++ vehicles/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Category, Vehicle
+
+# Register your models here.
+admin.register(Category)
+admin.register(Vehicle)
--- vehicles/apps.py
+++ vehicles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VeiculosConfig(AppConfig):
+class VehiclesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'veiculos'
+    name = 'vehicles'
